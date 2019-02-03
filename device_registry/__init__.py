fe12556ab4be5d4a4cb657e0fdf9a293d8203199
subodh101 <subodh.pushkar@gmail.com>
2019-02-03 15:50:24 +0900

    Addes Flask and flask_api

--- device_registry/__init__.py
+++ device_registry/__init__.py
@@ -1,10 +1,28 @@
 import markdown
 import os
+import shelve
+
 # Import the framework
-from flask import Flask
+from flask_restful import Resource, Api, reqparse
+from flask import Flask, g
 
 # Create an instance of flask
 app = Flask(__name__)
+
+# Create the api
+api = Api(app)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = shelve.open("devices.db")
+    return db
+
+@app.teardown_appcontext
+def teardown_db(execption):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 @app.route("/")
 def index():
@@ -18,3 +36,56 @@ def index():
 
         # Conver to HTML
         return markdown.markdown(content)
+
+class DeviceList(Resource):
+    def get(self):
+        shelf = get_db()
+        keys = list(shelf.keys())
+
+        devices = []
+
+        for key in keys:
+            devices.append(shelf[key])
+
+        return {'message': 'Success', 'date': devices}
+
+    def post(self):
+        parser = reqparse.RequestParser()
+
+        parser.add_argument('identifier', required=True)
+        parser.add_argument('name', required=True)
+        parser.add_argument('device_type', required=True)
+        parser.add_argument('controller_gateway', required=True)
+
+        # Parse the arguments into an object
+        args = parser.parse_args()
+
+        shelf = get_db()
+        shelf[args['identifier']] = args
+
+        return {'message': "Device registered", "data": args}, 201
+
+class Device(Resource):
+    """docstring for Device."""
+    def get(self, identifier):
+        shelf = get_db()
+
+        # If the key does not exist in the data store, return a 404 error
+        if not (identifier in shelf):
+            return {"message": "Device not found", 'data': {}}, 404
+
+        return {'message': 'Device found', 'data': shelf[identifier]}, 200
+
+    def delete(shef, identifier):
+        shelf = get_db()
+
+        # If the key does not exist in the data store, return a 404 error
+        if not (identifier in shelf):
+            return {'message': 'Device not found', 'data': {}}, 404
+
+        del shelf[identifier]
+        return '', 204
+
+
+api.add_resource(DeviceList, '/devices')
+api.add_resource(Device, '/device/<string:identifier>')
